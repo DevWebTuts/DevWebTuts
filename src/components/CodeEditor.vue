e62d39c966bc24cdc5035468e2e7f9427f7aa3d6
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-07-15 14:12:34 +0800

    Fixed Overflow in Code Editor

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -2,7 +2,7 @@
 v-container(fluid).pa-2
     v-layout(row wrap)
         v-flex(xs12 sm6)
-            code-mirror(v-model="code")
+            code-mirror.CodeEditorDialog(v-model="code")
         v-flex(xs12 sm6)
             iframe(:srcdoc="result", sandbox="allow-scripts allow-same-origin", 
             style="border: solid 2px; max-height: calc(100vh - 140px); height: calc(100vh - 140px);", 
